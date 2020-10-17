21b9430ea2d83d670b28e6fa43a9bd635623ee25
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-10-17 13:02:53 +0200

    Clear the input field when creating a new todo, add comments to js code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 
 
 let createTodo = () => {
+  // prevent reload
   event.preventDefault();
+  
+  // creates a unique id that will be used later to remove the elemnt when the todo is done
   let elementId = id();
+
+  /*
+    creates a div with a unique id
+    add the text from the input field
+    then creates a button that will call the function with the id wen clicked
+  */
   let todo = document.createElement("div");
   todo.setAttribute("id", elementId);
   let todoText = document.getElementById("todoText").value;
@@ -19,6 +28,10 @@ let createTodo = () => {
 
   let list = document.getElementById("todoList");
   list.appendChild(todo);
+
+
+  // reset form (empty the input text)
+  document.getElementById("todoForm").reset();
 }
 
 let handleTodo = (toRemove) => {
