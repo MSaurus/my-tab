4b16bb68c24e19e2ad2cb8c5a2affe65099af977
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-10-17 12:51:25 +0200

    Add the ability to create todos and remove them

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+function createTodo(){
+  event.preventDefault();
+  let elementId = id();
+  let todo = document.createElement("div");
+  todo.setAttribute("id", elementId);
+  let todoText = document.getElementById("todoText").value;
+  let text = document.createTextNode(todoText);
+  let button = document.createElement("button");
+  button.setAttribute("id", id());
+  button.setAttribute("onclick", "handleTodo("+elementId+")");
+
+  let buttonText = document.createTextNode("remove todo");
+  button.appendChild(buttonText);
+
+  todo.appendChild(text);
+  todo.appendChild(button);
+
+  let list = document.getElementById("todoList");
+  list.appendChild(todo);
+}
+
+function handleTodo(toRemove){
+  toRemove.remove();
+}
+
+function id () {
+  return '_' + Math.random().toString(36).substr(2, 9);
+};
