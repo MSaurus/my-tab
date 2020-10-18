be128b7dbfb45959b94e78f45603a5d3c876a484
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-10-18 13:02:55 +0200

    Add localstorage to feature, now when reloading all the tasks are still there

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+// so that we can use anywhere, will be used for pretty much everything
+let storage = window.localStorage;
 
 let createTodo = () => {
   // prevent reload
@@ -6,18 +8,50 @@ let createTodo = () => {
   
   // creates a unique id that will be used later to remove the elemnt when the todo is done
   let elementId = id();
+  let todoText = document.getElementById("todoText").value;
+  createTodoElement(elementId, todoText);
+  storage.setItem(elementId, todoText);
 
-  /*
-    creates a div with a unique id
-    add the text from the input field
-    then creates a button that will call the function with the id wen clicked
-  */
+  // reset form (empty the input text)
+  document.getElementById("todoForm").reset();
+}
+
+let handleTodo = (toRemove) => {
+  toRemove.remove();
+  storage.removeItem(toRemove.id);
+}
+
+
+// this should be good, because the user will probably no creae that many tasks
+let id = () => {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  loadLocalstorage();
+});
+
+let loadLocalstorage = () => {
+  for(let i = 0; i < storage.length; i++){
+    let elementId = storage.key(i);
+    let todoText = storage.getItem(elementId);
+    createTodoElement(elementId, todoText);
+  }
+}
+
+/*
+  creates a div with a unique id
+  add the text from the input field
+  then creates a button that will call the function with the id wen clicked
+*/
+let createTodoElement = (elementId, todoText) => {
+
   let todo = document.createElement("div");
   todo.setAttribute("id", elementId);
-  let todoText = document.getElementById("todoText").value;
+
   let text = document.createTextNode(todoText);
   let button = document.createElement("button");
-  button.setAttribute("id", id());
   button.setAttribute("onclick", "handleTodo("+elementId+")");
 
   let buttonText = document.createTextNode("remove todo");
@@ -28,16 +62,4 @@ let createTodo = () => {
 
   let list = document.getElementById("todoList");
   list.appendChild(todo);
-
-
-  // reset form (empty the input text)
-  document.getElementById("todoForm").reset();
-}
-
-let handleTodo = (toRemove) => {
-  toRemove.remove();
-}
-
-let id = () => {
-  return '_' + Math.random().toString(36).substr(2, 9);
 }
