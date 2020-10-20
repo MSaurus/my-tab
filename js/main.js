7f96f0f815d98b97c9c70337d4af075ab1e7a9f1
Mike Johansson <mike.johansson.mj@gmail.com>
2020-10-20 20:54:49 +0200

    Add functionality to edit username

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ document.addEventListener("DOMContentLoaded", function() {
   if (isUsernameSet()) {
     loadUsername()
   } else {
-    let usernameElement = document.querySelector(".user-name")
-    usernameElement.textContent = "Hello"
+    let usernameElement = document.querySelector(".username h1 span")
+    usernameElement.textContent = "User"
   }
 });
 
@@ -36,9 +36,9 @@ let isUsernameSet = () => {
 }
 
 let loadUsername = () => {
-  let usernameElement = document.querySelector(".user-name")
+  let usernameElement = document.querySelector(".username h1 span")
   let usernameInStorage = storage.getItem("username")
-  usernameElement.textContent = `Hello ${usernameInStorage}`
+  usernameElement.textContent = `${usernameInStorage}`
 }
 
 let createTodo = event => {
@@ -92,3 +92,34 @@ let createTodoElement = (elementId, todoText) => {
   let list = document.getElementById("todoList");
   list.appendChild(todo);
 }
+
+let usernameForm = document.querySelector("#usernameForm")
+
+/* fires when you press enter on the input
+** it gets the name you put in, saves it
+** in localstorage and the change the name
+** on screen
+*/
+let setUsername = event => {
+  event.preventDefault()
+  let newUsernameInput = document.querySelector("#newUsername")
+  if (newUsername.value != "") {
+    storage.setItem("username", newUsername.value)
+    let usernameElement = document.querySelector(".username h1 span")
+    usernameElement.textContent = `${newUsername.value}`
+    newUsernameInput.value = ""
+    usernameForm.setAttribute("hidden", "true")
+  } else {
+    usernameForm.setAttribute("hidden", "true")
+  }
+}
+
+usernameForm.addEventListener("submit", setUsername)
+
+
+let showInputField = () => {
+  usernameForm.removeAttribute("hidden")
+}
+
+let nameElement = document.querySelector(".username h1 span")
+nameElement.addEventListener("dblclick", showInputField)
