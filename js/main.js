1ec2dbdbeeabe7f42ea8eaba3d8990a5fb65b1c2
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-10-24 14:22:09 +0200

    Add semicolon and remove broken link tag

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ let handleTodo = (toRemove) => {
 }
 
 // Instead of onclick in html, add event listener to the form when submitting
-document.getElementById("todoForm").addEventListener('submit', createTodo)
+document.getElementById("todoForm").addEventListener('submit', createTodo);
 
 // this should be good, because the user will probably no create that many tasks
 let id = () => {
