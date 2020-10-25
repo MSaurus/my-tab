b54ad0d6e180a0a3620e89e5ebedb02fd89a7a6d
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-10-25 20:43:41 +0100

    The function is now called every one seconds and removed the seconds from the format, only shows hour and minutes now

--- js/main.js
+++ js/main.js
@@ -72,18 +72,15 @@ let showTime = () => {
   let date = new Date();
   let h = date.getHours(); // 0 - 23
   let m = date.getMinutes(); // 0 - 59
-  let s = date.getSeconds(); // 0 - 59
 
   
   h = (h < 10) ? "0" + h : h;
   m = (m < 10) ? "0" + m : m;
-  s = (s < 10) ? "0" + s : s;
   
-  let time = h + ":" + m + ":" + s;
-  document.getElementById("clock").innerText = time;
+  let time = h + ":" + m;
   document.getElementById("clock").textContent = time;
   
-  setTimeout(showTime,0);
+  setTimeout(showTime,1000);
   
 }
 
