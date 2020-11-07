57de97e807fba7a43ae531aaea02957006103910
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2020-11-07 14:41:11 +0100

    Move the clock widget logic into seperate js file

---
+++ js/clock.js
@@ -0,0 +1,15 @@
+let showTime = () => {
+  let date = new Date();
+  let h = date.getHours(); // 0 - 23
+  let m = date.getMinutes(); // 0 - 59
+
+  
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  
+  let time = h + ":" + m;
+  document.getElementById("clock").textContent = time;
+  
+  setTimeout(showTime,1000);
+  
+}
--- js/main.js
+++ js/main.js
@@ -102,23 +102,6 @@ let createTodoElement = (elementId, todoText) => {
   list.appendChild(todo);
 }
 
-let showTime = () => {
-  let date = new Date();
-  let h = date.getHours(); // 0 - 23
-  let m = date.getMinutes(); // 0 - 59
-
-  
-  h = (h < 10) ? "0" + h : h;
-  m = (m < 10) ? "0" + m : m;
-  
-  let time = h + ":" + m;
-  document.getElementById("clock").textContent = time;
-  
-  setTimeout(showTime,1000);
-  
-}
-
-
 /* fires when you press enter on the input
 ** it gets the name you put in, saves it
 ** in localstorage and the change the name
